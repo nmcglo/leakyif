86bd9141d6912a34a0b9c9082bafbd25e127c4fe
Neil McGlohon <nmcglohon@gmail.com>
2016-09-01 17:02:29 -0400

    Prototype of graph parser

---
+++ prototypes/graphMakeV01.m
@@ -0,0 +1,23 @@
+Layers = 3;
+
+Populations = [2,10,1];
+
+TotalNodes = sum(Populations);
+
+W = zeros(TotalNodes);
+
+for p = 1:length(Populations)-1
+    if(p == 1)
+        startndx = 1
+        endndx = sum(Populations(1:p))
+    else
+        startndx = sum(Populations(1:p-1))+1
+        endndx = sum(Populations(1:p))
+    end
+    
+    for i = startndx:endndx
+            for j = endndx+1:sum(Populations(1:p+1))
+                W(i,j) = 1;
+            end
+    end
+end
