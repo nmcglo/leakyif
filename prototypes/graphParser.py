8d85b30a2bf669461e76c221fff0e0c440175e4c
Neil McGlohon <nmcglohon@gmail.com>
2016-09-07 17:57:27 -0400

    Graph Parser Function
    graphParser is now a function that is imported for use, returns
    important values

--- prototypes/graphParser.py
+++ prototypes/graphParser.py
@@ -1,50 +1,58 @@
 import numpy as np
 
-infile = open('input.txt', 'r')
-
-print("Parsing the input file...")
-params = []
-with open('input.txt', 'r') as f:
-    for line in f:
-        s = line.split()
-        param = list(map(int,(s[1].split(','))))
-        params.append(param)
-
-print("Parameters: ",params)
-Layers = params[0][0]
-Populations = np.array(params[1])
-print("Layers: ", Layers)
-print("Populations:", Populations)
+weightMultiplier = 30
 
 
-TotalNodes = sum(Populations)
-print("Total Nodes: ",TotalNodes)
+def parseGraphInput(inputFilename):
 
-W = np.zeros((TotalNodes,TotalNodes))
-# print(W)
-print("Layers:")
-for p in range(0,len(Populations)):
-    if(p==0):
-        startndx = 0
-        endndx = sum(Populations[0:p+1]) -1
-    else:
-        startndx = sum(Populations[0:p])
-        endndx = sum(Populations[0:p+1]) -1
+    infile = open(inputFilename, 'r')
 
-    print(p,"\tstartndx:", startndx, "endndx:", endndx)
-    for i in range(startndx,endndx+1):
-        # print(i)
-        for j in range(endndx+1,sum(Populations[0:p+2])):
-            print("\t",i,j)
-            W[i,j] = 1;
+    print("Parsing the input file...")
+    params = []
+    with open('input.txt', 'r') as f:
+        for line in f:
+            s = line.split()
+            param = list(map(int,(s[1].split(','))))
+            params.append(param)
+
+    print("Parameters: ",params)
+    Layers = params[0][0]
+    Populations = np.array(params[1])
+    InputIDs = range(Populations[0])
+    print("Layers: ", Layers)
+    print("Populations:", Populations)
 
 
-print("\nAdjacency Matrix:")
-print(W)
+    TotalNodes = sum(Populations)
+    print("Total Nodes: ",TotalNodes)
 
-for i in range(0,np.shape(W)[0]):
-    for j in range(0,np.shape(W)[1]):
-        W[i,j] = W[i,j] * np.random.randn()
+    W = np.zeros((TotalNodes,TotalNodes))
+    # print(W)
+    print("Layers:")
+    for p in range(0,len(Populations)):
+        if(p==0):
+            startndx = 0
+            endndx = sum(Populations[0:p+1]) -1
+        else:
+            startndx = sum(Populations[0:p])
+            endndx = sum(Populations[0:p+1]) -1
 
-print("\nInteraction Matrix:")
-print(W)
+        print(p,"\tstartndx:", startndx, "endndx:", endndx)
+        for i in range(startndx,endndx+1):
+            # print(i)
+            for j in range(endndx+1,sum(Populations[0:p+2])):
+                print("\t",i,j)
+                W[i,j] = np.random.randn() * .25 + 1;
+
+
+    print("\nInteraction Matrix:")
+    print(W)
+    #
+    # for i in range(0,np.shape(W)[0]):
+    #     for j in range(0,np.shape(W)[1]):
+    #         W[i,j] = W[i,j] * np.random.randn()
+    #
+    # print("\nInteraction Matrix:")
+    # print(W)
+
+    return TotalNodes, Layers, Populations, InputIDs, W
